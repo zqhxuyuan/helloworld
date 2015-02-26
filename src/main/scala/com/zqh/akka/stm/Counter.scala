7f1d8eb7b3b6e6f63dc8676a22722e8f73edb26c
zqhxuyuan <zqhxuyuan@gmail.com>
2015-02-26 17:52:44 +0800

    ':) akka stm'

--- src/main/scala/com/zqh/akka/stm/Counter.scala
+++ src/main/scala/com/zqh/akka/stm/Counter.scala
@@ -1,14 +1,32 @@
 package com.zqh.akka.stm
 
+import akka.util.Timeout
+import akka.pattern.ask
+import akka.actor._
+import akka.transactor._
+import scala.concurrent.Await
+import scala.concurrent.stm._
+
 /**
  * Created by hadoop on 15-2-26.
+ *
+ * http://www.gtan.com/akka_doc/scala/transactors.html
  */
 object Counter extends App{
 
-  import akka.actor._
-  import akka.transactor._
-  import scala.concurrent.stm._
+  val system = ActorSystem("app")
 
+  val counter1 = system.actorOf(Props[Counter], name = "counter1")
+  val counter2 = system.actorOf(Props[Counter], name = "counter2")
+
+  implicit val timeout = Timeout(10)
+
+  counter1 ! Coordinated(Increment(Some(counter2)))
+
+  val count = Await.result(counter1 ? GetCount, timeout.duration)
+  println(count)
+
+  //---------------------------------------------------
   case class Increment(friend: Option[ActorRef] = None)
   case object GetCount
 
