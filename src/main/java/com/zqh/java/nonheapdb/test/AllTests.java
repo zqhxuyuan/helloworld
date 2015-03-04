349a539c184cd6439c79f092ab4189bddf792c55
zqhxuyuan <zqhxuyuan@gmail.com>
2015-03-04 17:36:29 +0800

    mcache

--- src/main/java/com/zqh/java/nonheapdb/test/AllTests.java
+++ src/main/java/com/zqh/java/nonheapdb/test/AllTests.java
@@ -5,8 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ByteBufferTest.class, CacheConcurrentTest.class,
-		CacheTest.class, VarintTest.class })
+@SuiteClasses({
+        ByteBufferTest.class,
+        CacheConcurrentTest.class,
+		CacheTest.class,
+        VarintTest.class
+})
 public class AllTests {
 
 }
