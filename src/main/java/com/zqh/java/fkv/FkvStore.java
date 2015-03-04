2a3974737f7ada17d6a2f9f4cfac31b6c0ab8481
zqhxuyuan <zqhxuyuan@gmail.com>
2015-03-04 17:50:54 +0800

    tkv and fkv

---
+++ src/main/java/com/zqh/java/fkv/FkvStore.java
@@ -0,0 +1,28 @@
+package com.zqh.java.fkv;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * @author sean.wang
+ * @since Nov 16, 2011
+ */
+public interface FkvStore {
+	byte[] get(int startIndex, int size);
+
+	void put(int startIndex, byte[] value);
+	
+	void put(int startIndex, byte value);
+
+	void close() throws IOException;
+
+	boolean isNeedDeserial();
+
+	ByteBuffer getBuffer();
+
+	void rewind();
+
+	int remaining();
+
+	void get(byte[] bytes);
+}
